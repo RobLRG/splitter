1b9728a21d50af91c121a1ef011728cfdb464424
RobLRG <bobpeters5889@hotmail.com>
2021-11-01 14:38:29 +0000

    Made it split and change color randomly

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 "use strict"
 
-alert("hello")
+
+function split4()
+{
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+
+
+}
+
+function addQuarter(intoDiv)
+{
+    let q = document.createElement("div")
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+
+    q.addEventListener("click",split4)
+
+    intoDiv.appendChild(q)
+
+    // let randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    // q.style.backgroundColor= "#" + randomColor
+
+    q.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+}
+
+document.body.addEventListener("click",split4)
